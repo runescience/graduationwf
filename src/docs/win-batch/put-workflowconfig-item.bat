ea14074258aa41f61891b39b906a85881cc40f16
jdsirag <joel.siragher@gmail.com>
2025-07-15 16:58:26 +0000

    converted files from winbatch to linux bash

--- src/docs/win-batch/put-workflowconfig-item.bat
+++ src/docs/win-batch/put-workflowconfig-item.bat
@@ -1,9 +1,9 @@
 @echo off
-set WORKFLOW_CONFIG_ID=WKFLCFG0001
-set WORKFLOW_NAME=Masters_Graduation_Workflow
-set WORKFLOW_APP_ID=WKFL0001
-set REVIEWER1=reviewer1
-set REVIEWER2=reviewer2
+set WORKFLOW_CONFIG_ID=WKFLCFG0002
+set WORKFLOW_NAME=Harvesting_apples_workflow
+set WORKFLOW_APP_ID=WKFL0002
+set REVIEWER1=reviewer3
+set REVIEWER2=reviewer4
 set WF_DEFINITION_ARN=ARN-111-222-678
 set FORMLY_CONFIG_BLOB=this is a formly blob3
 
